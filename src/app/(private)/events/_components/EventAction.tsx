3fa75dd1e2fbcec0addd1b068c6b019bf8e9f8a5
amrita <maharjanamrita94@gmail.com>
2025-05-21 16:44:53 +0545

    redone checkbox addition

--- src/app/(private)/events/_components/EventAction.tsx
+++ src/app/(private)/events/_components/EventAction.tsx
@@ -3,6 +3,7 @@
 import ApiClient from "@/api-client/";
 import { TooltipWrapper } from "@/components/TooltipWrapper";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Select,
   SelectContent,
@@ -90,6 +91,7 @@ const EventAction = ({ event }: { event: EventType }) => {
           updateEventConfigId.isPending ? "inline-flex" : "invisible"
         }`}
       />
+      <Checkbox></Checkbox>
       <TooltipWrapper content={"Delete"}>
         <Button
           variant="outline"
